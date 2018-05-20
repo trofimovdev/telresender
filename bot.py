7f918eed275965286209c03efb6945df3fad6959
Bixnel <hi@bixnel.ru>
2018-05-20 15:29:09 +0300

    :checkered_flag:

--- bot.py
+++ bot.py
@@ -9,11 +9,11 @@ bot = telebot.TeleBot(config.token)
 	
 @bot.message_handler(commands=["start"])
 def start(message):
-	bot.send_message(message.chat.id, 'Работаю 24 часа, 7 дней в неделю, 365 дней в году. \n\nНапиши /help для помощи.')
+	bot.send_message(message.chat.id, 'Hello. \n\n Write /help fro help.')
 
 @bot.message_handler(commands=["help"])
 def start(message):
-	bot.send_message(message.chat.id, 'Как пользоваться? Очень просто. \n\nПросто отправь мне сообщение, а когда мой хозяин ответит, я автоматически перешлю его тебе. \n\n Ах, да. Если ты мне ничего не напишешь, то я ничего ему не отправлю😈')
+	bot.send_message(message.chat.id, 'Help message😈')
 
 
 
@@ -32,7 +32,7 @@ def messages(message):
 			pass
 	else:
 		bot.send_message(config.owner, str(message.chat.id) + ': ' + message.text)
-		bot.send_message(message.chat.id, '%s, Ваш запрос принят. Bastax уже получил его. Ожидайте 👍'%message.chat.username)
+		bot.send_message(message.chat.id, '%s, wait please 👍'%message.chat.username)
 
 if __name__ == '__main__':
-	bot.polling(none_stop=True)
+	bot.polling(none_stop = True)
